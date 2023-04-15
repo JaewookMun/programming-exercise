9cf2f4ae652546bc0051fc6bee97495e8fe4b208
JaewookMun <mun1103.dev@gmail.com>
2023-04-15 18:48:37 +0900

    update files

--- max-session/src/main/java/example/security/maxsession/auth/AuthenticationManager.java
+++
@@ -1,8 +0,0 @@
-package example.security.maxsession.auth;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-
-public interface AuthenticationManager {
-    Authentication authenticate(Authentication authentication) throws AuthenticationException;
-}
