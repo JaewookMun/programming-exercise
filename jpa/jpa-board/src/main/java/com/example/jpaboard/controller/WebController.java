0b98b1cc646864aaab43c520d60e859b9d89bbee
JaewookMun <mun1103.dev@gmail.com>
2023-07-09 16:14:02 +0900

    1. [Front-End] update

--- jpa/jpa-board/src/main/java/com/example/jpaboard/controller/WebController.java
+++ jpa/jpa-board/src/main/java/com/example/jpaboard/controller/WebController.java
@@ -5,7 +5,9 @@ import com.example.jpaboard.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -17,6 +19,13 @@ public class WebController {
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String boardDetail(@PathVariable(name = "id", required = true) Long id, Model model) {
+        model.addAttribute("id", id);
+
+        return "boardDetail";
     }
 
     @GetMapping("init")
