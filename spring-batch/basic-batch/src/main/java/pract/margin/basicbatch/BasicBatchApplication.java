ca40a2744823f8a03e8f7f6b86ab679ab1359a33
JaewookMun <mun1103.dev@gmail.com>
2023-08-21 19:49:50 +0900

    first commit for spring batch guid

---
+++ spring-batch/basic-batch/src/main/java/pract/margin/basicbatch/BasicBatchApplication.java
@@ -0,0 +1,14 @@
+package pract.margin.basicbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicBatchApplication.class, args);
+//		System.exit(SpringApplication.exit(SpringApplication.run(BasicBatchApplication.class, args)));
+	}
+
+}
