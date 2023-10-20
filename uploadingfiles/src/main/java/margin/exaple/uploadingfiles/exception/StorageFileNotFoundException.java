0b980bf7bb42cd0b9fc6d490d70bfd04d24c1b25
JaewookMun <mun1103.dev@gmail.com>
2023-10-20 21:14:41 +0900

    1. Change location

--- uploadingfiles/src/main/java/margin/exaple/uploadingfiles/exception/StorageFileNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package margin.exaple.uploadingfiles.exception;
-
-public class StorageFileNotFoundException extends Throwable {
-    public StorageFileNotFoundException() {
-        super();
-    }
-}
