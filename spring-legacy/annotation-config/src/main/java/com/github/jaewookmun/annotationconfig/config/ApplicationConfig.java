7a8fc628f7fd607e380861b78b1d369b0a1bd006
JaewookMun <mun1103.dev@gmail.com>
2024-08-23 17:55:05 +0900

    1. Spring project setup

---
+++ spring-legacy/annotation-config/src/main/java/com/github/jaewookmun/annotationconfig/config/ApplicationConfig.java
@@ -0,0 +1,11 @@
+package com.github.jaewookmun.annotationconfig.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * DataSource와 같은 기능 Bean 등록
+ */
+@Configuration
+public class ApplicationConfig {
+
+}
---
+++ spring-legacy/annotation-config/src/main/java/com/github/jaewookmun/annotationconfig/config/ServletConfig.java
@@ -0,0 +1,13 @@
+package com.github.jaewookmun.annotationconfig.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+@EnableWebMvc
+//public class ServletConfig extends WebMvcConfigurerAdapter {
+public class ServletConfig {
+
+}
