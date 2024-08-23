daf2cd6c0cf8c63f1c18f7241bddd6f592dea03a
JaewookMun <mun1103.dev@gmail.com>
2024-08-23 18:03:23 +0900

    1. basic setup for componentscan

--- spring-legacy/annotation-config/src/main/java/com/github/jaewookmun/annotationconfig/config/ServletConfig.java
+++ spring-legacy/annotation-config/src/main/java/com/github/jaewookmun/annotationconfig/config/ServletConfig.java
@@ -1,5 +1,6 @@
 package com.github.jaewookmun.annotationconfig.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -7,7 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 @EnableWebMvc
+@ComponentScan(
+        basePackages = "com.github.jaewookmun.annotationconfig"
+)
 //public class ServletConfig extends WebMvcConfigurerAdapter {
-public class ServletConfig {
+public class ServletConfig
+{
 
 }
---
+++ spring-legacy/annotation-config/src/main/java/com/github/jaewookmun/annotationconfig/controller/SampleController.java
@@ -0,0 +1,19 @@
+package com.github.jaewookmun.annotationconfig.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SampleController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String hello()
+    {
+
+        return "hello";
+    }
+
+}
