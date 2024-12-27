8a0114f3258c38dc334eb42e773632ce9cb6a864
JaewookMun <mun1103.dev@gmail.com>
2024-12-27 14:59:45 +0900

    first commit for Mybatis Dynamic SQL

---
+++ mybatis/mybatis-dynamic-sql/src/main/java/com/github/jaewookmun/mybatisdynamicsql/MybatisDynamicSqlApplication.java
@@ -0,0 +1,13 @@
+package com.github.jaewookmun.mybatisdynamicsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisDynamicSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisDynamicSqlApplication.class, args);
+	}
+
+}
---
+++ mybatis/mybatis-dynamic-sql/src/test/java/com/github/jaewookmun/mybatisdynamicsql/MybatisDynamicSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jaewookmun.mybatisdynamicsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisDynamicSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
