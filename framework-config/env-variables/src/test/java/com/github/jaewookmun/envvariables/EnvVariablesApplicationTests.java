3edb3c0e5cb0bdc632e45311242e2fec3ec1a9a4
JaewookMun <mun1103.dev@gmail.com>
2025-05-11 16:44:11 +0900

    init commit

---
+++ framework-config/env-variables/src/test/java/com/github/jaewookmun/envvariables/EnvVariablesApplicationTests.java
@@ -0,0 +1,14 @@
+package com.github.jaewookmun.envvariables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvVariablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
