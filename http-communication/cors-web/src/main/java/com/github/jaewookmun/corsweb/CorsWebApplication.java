3d6919b6bfd4a184f71c86a6af6dee9bce42ba0e
JaewookMun <mun1103.dev@gmail.com>
2025-09-09 20:56:09 +0900

    commit first
    - cors-web

---
+++ http-communication/cors-web/src/main/java/com/github/jaewookmun/corsweb/CorsWebApplication.java
@@ -0,0 +1,13 @@
+package com.github.jaewookmun.corsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorsWebApplication.class, args);
+	}
+
+}
---
+++ http-communication/cors-web/src/test/java/com/github/jaewookmun/corsweb/CorsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jaewookmun.corsweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorsWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
