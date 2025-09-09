1b2fa600d967ca53d11624f44470097a61a11219
JaewookMun <mun1103.dev@gmail.com>
2025-09-09 20:56:41 +0900

    commit first
    - cors-resources

---
+++ http-communication/cors-resources/src/main/java/com/github/jaewookmun/corsresources/CorsResourcesApplication.java
@@ -0,0 +1,13 @@
+package com.github.jaewookmun.corsresources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsResourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorsResourcesApplication.class, args);
+	}
+
+}
---
+++ http-communication/cors-resources/src/test/java/com/github/jaewookmun/corsresources/CorsResourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jaewookmun.corsresources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorsResourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
